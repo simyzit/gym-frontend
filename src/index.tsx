0763495103fe5f743e9079bc6c48b67ccf42a03f
Max Lisicin <maxlisicin@Maxs-MacBook-Air.local>
2023-10-23 21:59:33 +0300

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import {persistor, store} from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <HashRouter basename='/'>
   <Provider store={store}>
     <BrowserRouter basename='/gym-frontend/'>
     <PersistGate loading={null} persistor={persistor}>
@@ -19,5 +20,6 @@ root.render(
     </PersistGate>  
     </BrowserRouter>
   </Provider>
+  </HashRouter>
 );
 
