17beaabfffc986230fb44449149a9a60dc95bb4d
Max Lisicin <maxlisicin@Maxs-MacBook-Air.local>
2023-10-23 20:10:58 +0300

    changes

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://application-gym.onrender.com/api",
 });
 
-process.env.REACT_APP_API_URL;
+// process.env.REACT_APP_API_URL
